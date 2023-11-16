819843e58444630681a9eead2642d1fea5963a57
hiroya <hiroya3508@gmail.com>
2023-11-16 20:54:34 +0900

    update

--- ABC328/A.py
+++ ABC328/A.py
@@ -1,9 +1,14 @@
-N, X = map(int, input().split())
-S = list(map(int, input().split()))
 
-total = 0
-for i in range(N):
-    if S[i] <= X:
-        total += S[i]
+def my_answer():
+    N, X = map(int, input().split())
+    S = list(map(int, input().split()))
 
-print(total)
+    total = 0
+    for i in range(N):
+        if S[i] <= X:
+            total += S[i]
+
+    print(total)
+
+if __name__ == '__main__':
+    my_answer()
