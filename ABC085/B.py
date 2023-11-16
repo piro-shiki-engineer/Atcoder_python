f620e6e81c9ca7a80071fef98c73ade1eb6d5bc7
hiroya <hiroya3508@gmail.com>
2023-11-16 22:45:45 +0900

    重複を防ぐ集合を意味するset型使うとより短く実装できる

--- ABC085/B.py
+++ ABC085/B.py
@@ -1,23 +1,11 @@
 
 def my_answer():
     N = int(input())
-    d = [0]*N
-    for i in range(N):
-        d[i] = int(input())
+    count = set()
+    for _ in range(N):
+        count.add(int(input()))
 
-    d.sort(reverse=True)
-    count = 1
-    tmp = d[0]
-
-    for i in range(1, N):
-        if d[i] < tmp:
-            count += 1
-            tmp = d[i]
-        else:
-            tmp = d[i]
-            continue
-
-    print(count)
+    print(len(count))
 
 
 if __name__ == '__main__':
