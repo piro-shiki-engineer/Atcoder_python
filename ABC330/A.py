7e99973fd9a4e9b29699894c4e8a09c51f4a7062
hiroya <hiroya3508@gmail.com>
2023-11-26 02:52:42 +0900

    簡単は条件問題

---
+++ ABC330/A.py
@@ -0,0 +1,14 @@
+def my_answer():
+    N, L, R= map(int, input().split())
+    A = list(map(int, input().split()))
+    count = 0
+
+    for i in range(N):
+        if A[i] >= l
+            count += 1
+
+    print(count)
+
+
+if __name__ == '__main__':
+    my_answer()
