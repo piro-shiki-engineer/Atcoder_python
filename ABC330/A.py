47a4c580e60ee794f26ed44d6da50412ec246a46
hiroya <hiroya3508@gmail.com>
2023-11-27 00:35:28 +0900

    A問題回答できた

--- ABC330/A.py
+++ ABC330/A.py
@@ -1,10 +1,10 @@
 def my_answer():
-    N, L, R= map(int, input().split())
+    N, L = map(int, input().split())
     A = list(map(int, input().split()))
     count = 0
 
     for i in range(N):
-        if A[i] >= l
+        if A[i] >= L:
             count += 1
 
     print(count)
