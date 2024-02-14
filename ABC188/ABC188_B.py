66499442b3460ac903a9330abb367faece286861
hiroya <hiroya3508@gmail.com>
2024-02-15 03:51:59 +0900

    B問題回答できた

---
+++ ABC188/ABC188_B.py
@@ -0,0 +1,16 @@
+def my_answer():
+    N = int(input())
+    A_vector = list(map(int , input().split()))
+    B_vector = list(map(int , input().split()))
+    Dot_val = 0
+
+    for i in range(N):
+        Dot_val += A_vector[i]*B_vector[i]
+
+    if Dot_val == 0:
+        print("Yes")
+    else:
+        print("No")
+
+if __name__ == '__main__':
+    my_answer()
