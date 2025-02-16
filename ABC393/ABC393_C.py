ee3fa18c17a9ffcb52cf5a0bf2a7097140efe497
hiroya.h <hiroya3508@gmail.com>
2025-02-16 17:30:00 +0900

    C問題回答_best

--- ABC393/ABC393_C.py
+++ ABC393/ABC393_C.py
@@ -1,18 +1,18 @@
 def main():
     n, m = map(int, input().split())
-    matrix = [[False]*n for _ in range(n)]
+    edges = {}
     result = 0
 
     for i in range(m):
         u_i, v_i = map(int, input().split())
         u_i, v_i = u_i-1, v_i-1
-        if matrix[u_i][v_i] == True or u_i == v_i:
+        if edges.get((u_i, v_i), False) or u_i == v_i:
             result += 1
         else:
-            matrix[u_i][v_i] = True
-            matrix[v_i][u_i] = True
-
+            edges[(u_i, v_i)] = True
+            edges[(v_i, u_i)] = True
     print(result)
+
 
 if __name__ == '__main__':
     main()
